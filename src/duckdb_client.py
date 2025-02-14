daa5e6a36b956f4bd14e3c89ac61870f70121fba
Clemens Rietdorf <c.rietdorf@dnb.de>
2025-02-14 09:30:11 +0100

    integrated duckdb in ebm4subjects

---
+++ src/duckdb_client.py
@@ -0,0 +1,291 @@
+from threading import Thread
+import duckdb
+import pandas as pd
+
+
+class Duckdb_client:
+    def __init__(
+        self,
+        db_path: str,
+        read_only: bool = False,
+        config: dict = {},
+    ) -> None:
+        self.connection = duckdb.connect(
+            database=db_path,
+            read_only=read_only,
+            config=config,
+        )
+
+        self.connection.install_extension("vss")
+        self.connection.load_extension("vss")
+        self.connection.install_extension("fts")
+        self.connection.load_extension("fts")
+
+    def create_collection(
+        self,
+        collection_df: pd.DataFrame,
+        collection_name: str = "gnd",
+        vector_dimensions: int = 1024,
+        hnsw_index_name: str = "hnsw_index",
+        hnsw_metric: str = "l2sq",
+        fts_config: dict = {},
+        force: bool = False,
+    ):
+        if isinstance(force, str):
+            force = force == "true" or force == "True"
+
+        replace = ""
+        if force:
+            replace = "OR REPLACE "
+
+        print(f"Creating/overwriting collection {collection_name}")
+        self.connection.execute(
+            f"""CREATE {replace}TABLE {collection_name} (
+                id INTEGER,
+                idn VARCHAR,
+                label_text VARCHAR,
+                is_prefLabel BOOLEAN,
+                embeddings FLOAT[{vector_dimensions}])"""
+        )
+
+        print(f"Inserting vocabulary into {collection_name}")
+        self.connection.execute(
+            f"INSERT INTO {collection_name} BY NAME SELECT * FROM collection_df"
+        )
+
+        print("Creating/overwriting HNSW-index")
+        if force:
+            self.connection.execute(f"DROP INDEX IF EXISTS {hnsw_index_name}")
+        self.connection.execute(
+            f"""CREATE INDEX IF NOT EXISTS {hnsw_index_name}
+            ON {collection_name}
+            USING HNSW (embeddings)
+            WITH (metric = '{hnsw_metric}')"""
+        )
+
+        print("Creating/overwriting FTS-index")
+        self.connection.execute(f"""PRAGMA create_fts_index(
+                                '{collection_name}', 
+                                'id', 
+                                'label_text',
+                                overwrite={int(force)},
+                                stemmer='{fts_config.get("stemmer", "porter")}',
+                                stopwords='{fts_config.get("stopwords", "none")}',
+                                ignore='{fts_config.get("ignore", "[^a-z]+")}',
+                                strip_accents={fts_config.get("strip_accents", 1)},
+                                lower={fts_config.get("lower", 1)})""")
+
+    def query_collection(
+        self,
+        query_df: pd.DataFrame,
+        collection_name: str,
+        vector_dimensions: int,
+        n_jobs: int = 1,
+        n_hits: int = 10,
+        alpha: float = 0.5,
+        hnsw_metric_function: str = "array_distance",
+    ):
+        result_list_fts = []
+        result_list_vss = []
+
+        query_ids = query_df["id"].tolist()
+        query_doc_ids = query_df["doc_id"].tolist()
+        query_chunk_positions = query_df["chunk_position"].tolist()
+        query_texts = query_df["text"].tolist()
+
+        chunk_size = max(int(len(query_ids) / n_jobs), 1)
+        chunked_query_ids = [
+            query_ids[i : i + chunk_size] for i in range(0, len(query_ids), chunk_size)
+        ]
+        chunked_doc_ids = [
+            query_doc_ids[i : i + chunk_size]
+            for i in range(0, len(query_doc_ids), chunk_size)
+        ]
+        chunked_chunk_positions = [
+            query_chunk_positions[i : i + chunk_size]
+            for i in range(0, len(query_chunk_positions), chunk_size)
+        ]
+        chunked_query_texts = [
+            query_texts[i : i + chunk_size]
+            for i in range(0, len(query_texts), chunk_size)
+        ]
+
+        threads = [
+            Thread(
+                target=self.__vss_thread_query,
+                args=(
+                    query_df,
+                    result_list_vss,
+                    collection_name,
+                    vector_dimensions,
+                    hnsw_metric_function,
+                    n_hits,
+                ),
+            )
+        ]
+        for ids, doc_ids, chunk_positions, texts in zip(
+            chunked_query_ids,
+            chunked_doc_ids,
+            chunked_chunk_positions,
+            chunked_query_texts,
+        ):
+            threads.append(
+                Thread(
+                    target=self.__fts_thread_query,
+                    args=(
+                        collection_name,
+                        ids,
+                        doc_ids,
+                        chunk_positions,
+                        texts,
+                        result_list_fts,
+                        n_hits,
+                    ),
+                )
+            )
+            pass
+
+        for thread in threads:
+            thread.start()
+        for thread in threads:
+            thread.join()
+
+        results_fts = pd.concat(result_list_fts, ignore_index=True)
+
+        results_vss = (
+            result_list_vss[0]
+            .explode(["is_prefLabels", "label_texts", "vss_matches", "vss_scores"])
+            .reset_index(drop=True)
+            .rename(
+                columns={
+                    "is_prefLabels": "is_prefLabel",
+                    "label_texts": "label_text",
+                    "vss_matches": "match",
+                    "vss_scores": "vss_score",
+                }
+            )
+        )
+
+        results_vss["vss_score"] = (
+            results_vss["vss_score"] / results_vss["vss_score"].max()
+        )
+        results_fts["fts_score"] = (
+            results_fts["fts_score"] / results_fts["fts_score"].max()
+        )
+
+        results = (
+            results_vss.join(
+                other=results_fts.set_index(
+                    [
+                        "id",
+                        "doc_id",
+                        "chunk_position",
+                        "match",
+                        "is_prefLabel",
+                        "label_text",
+                    ]
+                ),
+                on=[
+                    "id",
+                    "doc_id",
+                    "chunk_position",
+                    "match",
+                    "is_prefLabel",
+                    "label_text",
+                ],
+                how="outer",
+            )
+            .infer_objects(copy=False)
+            .fillna(0)
+            .rename(columns={"match": "label_id"})
+        )
+
+        results["score"] = (
+            results["vss_score"] * (1 - alpha) + results["fts_score"] * alpha
+        )
+
+        return results
+
+    def __vss_thread_query(
+        self,
+        queries_df: pd.DataFrame,
+        results: list,
+        collection_name: str,
+        vector_dimensions: int,
+        hnsw_metric_function: str = "array_distance",
+        limit: int = 10,
+    ):
+        thread_connection = self.connection.cursor()
+
+        thread_connection.execute(
+            f"""CREATE OR REPLACE TABLE queries ( 
+                id INTEGER,
+                doc_id VARCHAR,
+                chunk_position INTEGER,
+                text VARCHAR,
+                embeddings FLOAT[{vector_dimensions}])"""
+        )
+
+        thread_connection.execute(
+            "INSERT INTO queries BY NAME SELECT * FROM queries_df"
+        )
+
+        result = thread_connection.execute(
+            f"""SELECT queries.id, 
+            queries.doc_id,
+            queries.chunk_position,
+            list(idn) AS vss_matches,
+            list(is_prefLabel) AS is_prefLabels,
+            list(label_text) AS label_texts,
+            list(vss_score) AS vss_scores 
+            FROM queries, LATERAL (
+                SELECT {collection_name}.idn,
+                {collection_name}.is_prefLabel,
+                {collection_name}.label_text,
+                {hnsw_metric_function}(queries.embeddings, {collection_name}.embeddings) AS vss_score
+                FROM {collection_name}
+                ORDER BY vss_score
+                LIMIT {limit}
+            )
+            GROUP BY id, doc_id, chunk_position"""
+        ).df()
+
+        results.append(result)
+
+    def __fts_thread_query(
+        self,
+        collection_name,
+        query_ids: list[int],
+        doc_ids: list[str],
+        chunk_positions: list[int],
+        query_texts: list[str],
+        results: list,
+        limit: int = 10,
+    ):
+        thread_connection = self.connection.cursor()
+
+        thread_connection.execute(f"""PREPARE query AS (
+                                  WITH scored_docs AS (
+                                    SELECT *, fts_main_{collection_name}.match_bm25(id, ?, fields := 'label_text') AS fts_score 
+                                    FROM {collection_name}
+                                  )
+                                  SELECT idn AS match, is_prefLabel, label_text, fts_score
+                                  FROM scored_docs
+                                  WHERE fts_score IS NOT NULL
+                                  ORDER BY fts_score DESC
+                                  LIMIT {limit})
+                                  """)
+
+        for query_id, doc_id, chunk_position, query_text in zip(
+            query_ids, doc_ids, chunk_positions, query_texts
+        ):
+            query_text = query_text.replace("'", "")
+            query_text = query_text.replace('"', "")
+
+            result = thread_connection.execute(f"EXECUTE query('{query_text}')").df()
+
+            result["id"] = query_id
+            result["doc_id"] = doc_id
+            result["chunk_position"] = chunk_position
+
+            results.append(result)
--- src/index_corpus.py
+++ src/index_corpus.py
@@ -1,216 +1,137 @@
-import weaviate
-import weaviate.classes as wvc
-from weaviate.classes.query import Filter
-import torch
-import pandas as pd
-import numpy as np
-import requests
-from weaviate.classes.query import MetadataQuery
-import sys
 import argparse
-import multiprocessing
-from tqdm import tqdm
-import time
-import hashlib
-from dvc.api import params_show
 import logging
-from functools import partial
 
-# Create argument parser
-parser = argparse.ArgumentParser(description='Process command line arguments')
+import numpy as np
+import pandas as pd
+from dvc.api import params_show
 
-class Indexer:
+from duckdb_client import Duckdb_client
 
-    def __init__(self, alpha, n_hits, top_k, vocab_collection):
-        self.alpha = alpha
-        self.n_hits = n_hits
-        self.top_k = top_k
-        self.vocab_collection = vocab_collection
+params = params_show()
+embedding_dim = params["general"]["embedding_dim"]
+collection_name = params["vocab_config"]["collection_name"]
 
-    def index_chunk(
-            self,
-            text_query: str, 
-            embedding: list,
-            doc_id: str, 
-            client: weaviate.Client, 
-    ):
-        vocab_collection = client.collections.get(self.vocab_collection)
-
-        response = vocab_collection.query.hybrid(
-            query=text_query,
-            vector=embedding,
-            limit=self.n_hits,
-            return_metadata=MetadataQuery(score=True),
-            alpha=self.alpha
-        )
-
-        df = pd.DataFrame(
-            [
-                dict(
-                    doc_id=doc_id,
-                    label_id=o.properties['idn'],
-                    score=o.metadata.score,
-                    is_prefLabel=o.properties['is_prefLabel']
-                )
-                for o in response.objects
-            ]
-        )
-
-        # group by label_id and only keep rows with the highest score per label_id
-        if not df.empty:
-            df = df.sort_values('score', ascending=False).groupby('label_id').head(1)
-            return df
-        else:
-            return {}
-
-    def index_text(self, chunks: list, embeddings: list, doc_id: str, client: weaviate.Client):
-        # Split the text into chunks of 1000 characters
-        candidates = pd.DataFrame(columns=['doc_id', 'label_id', 'score', 'chunk_position', 'is_prefLabel'])
-        n_chunks = len(chunks)
-        for i in range(n_chunks):
-            # skip 1-word chunks
-            #if len(chunks[i].split()) <= 1:
-            #    continue
-            chunk_df = self.index_chunk(
-                text_query = chunks[i],
-                embedding = embeddings[i], 
-                doc_id = doc_id, 
-                client = client)
-            if not chunk_df.empty:
-                chunk_df['chunk_position'] = i/n_chunks
-                if not candidates.empty:
-                    candidates = pd.concat([candidates, chunk_df])
-                else:
-                    candidates = chunk_df
-
-        df = candidates.groupby('label_id').agg(
-            score=('score', 'sum'),
-            occurrences=('doc_id', 'count'),
-            first_occurence=('chunk_position', 'min'),
-            last_occurence=('chunk_position', 'max'),
-            spread=('chunk_position', lambda x: x.max() - x.min()),
-            is_prefLabel=('is_prefLabel', 'any')
-        ).reset_index()
-        df.columns = ['label_id', 'score', 'occurrences', 'first_occurence', 'last_occurence', 'spread', 'is_prefLabel']
-        # normalize score by n_chunks
-        df['score'] = df['score'] / n_chunks
-        # add new column doc_id at as first column, filled with the constand value `doc_id` as passed to the function
-        df.insert(0, 'doc_id', doc_id)
-        # arrange by score and keep top K
-        df = df.sort_values('score', ascending=False).head(self.top_k)
-
-        return df
-
-# Define the function to be executed in parallel
-def process_document(item, client: weaviate.Client, indexer: Indexer):
-    if not isinstance(item, dict):
-        raise ValueError("Row must be an instance of dict")
-    chunks = item['chunks']
-    embeddings = item['embeddings']
-    doc_id = item['doc_id']
-
-    # print(f"Processing document {doc_id}")
-    try:
-        return indexer.index_text(chunks=chunks,
-                          embeddings=embeddings, 
-                          doc_id = doc_id, 
-                          client = client)
-    except Exception as e:
-        print("An error occurred in doc_id", doc_id, str(e))
-        return None
-
-# Define the function to process a batch of documents
-def process_batch(batch, position, indexer: Indexer):
-    # batch_id = multiprocessing.current_process()._identity[0]
-    # print(f"Processing batch {batch_id}")
-    client = weaviate.connect_to_local()
-    if not client.is_ready():
-      sys.exit("Weaviate client is not ready. Exiting...")
-    result = []
-    for item in (pbar := tqdm(batch, total=len(batch), position=position, leave=True)):
-        pbar.set_description(f"{position:02}")
-        result.append(
-            process_document(item, client, indexer))
-    client.close()
-    return result
-
-if __name__ == '__main__':
-    # Add arguments with default values
-    parser.add_argument('--chunks', type=str, default='results/test/chunks.txt', help='Text chunks to be indexed')
-    parser.add_argument('--chunk_index', type=str, default='results/test/chunk_index.arrow', help='Index file containing a mapping of documents and chunks')
-    parser.add_argument('--chunk_embeddings', type=str, default='results/test/embeddings.arrow', help='Embeddings file matching the chunks and chunk_index')
-    parser.add_argument('--alpha', type=float, default=0.0, help='Alpha value for hybrid search')
-    parser.add_argument('--top_k', type=int, default=100, help='Number of top k results to keep')
-    parser.add_argument('--n_hits', type=int, default=20, help='Number of hits to retrieve from Hybrid Search')
-    parser.add_argument('--n_jobs', type=int, default=20, help='Number of parallel jobs')
-    parser.add_argument('--output', type=str, default='results/test/candidates.arrow', help='Output file')
-
-    # Parse arguments
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process command line arguments")
+    parser.add_argument(
+        "--db_path", type=str, default="gnd.duckdb", help="Path to DuckDB"
+    )
+    parser.add_argument(
+        "--chunks",
+        type=str,
+        default="results/test/chunks.txt",
+        help="Text chunks to be indexed",
+    )
+    parser.add_argument(
+        "--chunk_index",
+        type=str,
+        default="results/test/chunk_index.arrow",
+        help="Index file containing a mapping of documents and chunks",
+    )
+    parser.add_argument(
+        "--chunk_embeddings",
+        type=str,
+        default="results/test/chunk_embeddings.npy",
+        help="Embeddings file matching the chunks and chunk_index",
+    )
+    parser.add_argument(
+        "--alpha", type=float, default=0.5, help="Alpha value for hybrid search"
+    )
+    parser.add_argument(
+        "--top_k", type=int, default=100, help="Number of top k results to keep"
+    )
+    parser.add_argument(
+        "--n_hits",
+        type=int,
+        default=10,
+        help="Number of hits to retrieve from Hybrid Search",
+    )
+    parser.add_argument(
+        "--n_jobs", type=int, default=20, help="Number of parallel jobs"
+    )
+    parser.add_argument(
+        "--output",
+        type=str,
+        default="results/test/candidates_new.arrow",
+        help="Output file",
+    )
     args = parser.parse_args()
 
-    # Set up logging
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
     logger = logging.getLogger(__name__)
 
-    # Load parameters from params.yaml
-    params = params_show()
+    client = Duckdb_client(
+        db_path=args.db_path,
+        config={"hnsw_enable_experimental_persistence": True},
+    )
 
-    indexer = Indexer(
-        alpha=args.alpha, n_hits=args.n_hits, top_k=args.top_k, 
-        vocab_collection=params["vocab_config"]["collection_name"]
-        )
+    logger.info("Reading chunks...")
+    with open(args.chunks, "r", encoding="utf-8") as file:
+        chunk_texts = file.readlines()
 
-    n_jobs = args.n_jobs
-    output = args.output
-
-
-    logger.info("Reading chunks and chunk index...")
-    # Get the data from previous stages chunk_texts and embed_chunks
-    with open(args.chunks, 'r', encoding='utf-8') as file:
-        chunks = file.readlines()
+    logger.info("Reading chunk index...")
     chunk_index = pd.read_feather(args.chunk_index)
+    chunk_positions = chunk_index["rel_chunk_position"].tolist()
+    doc_ids = chunk_index["doc_id"].tolist()
+    n_chunks_df = (
+        chunk_index.groupby("doc_id")
+        .agg(n_chunks=("rel_chunk_position", "count"))
+        .reset_index()
+    )
+
     logger.info("Reading chunk embeddings...")
     chunk_embdeddings = np.load(args.chunk_embeddings)
-    docs_w_emb = []
 
-    logger.info("Join chunk embeddings with chunk texts...")
-    # Iterate over unique doc_ids in the chunk index
-    for doc_id in chunk_index['doc_id'].unique():
-        # Get the chunks and embeddings for the current doc_id
-        this_docs_chunk_ids = chunk_index[chunk_index['doc_id'] == doc_id]['abs_chunk_position'].tolist()
-        start = this_docs_chunk_ids[0]
-        end = this_docs_chunk_ids[-1] + 1
-        doc_chunks = chunks[start:end]
-        doc_embeddings = chunk_embdeddings[start:end, :]
-        # Create a dictionary for the current document
-        doc_item = {
-            'doc_id': doc_id,
-            'chunks': doc_chunks,
-            'embeddings': doc_embeddings
-        }
-        # Append the document item to the list
-        docs_w_emb.append(doc_item)
+    input_df = pd.DataFrame()
+    input_df["id"] = [i for i in range(len(chunk_texts))]
+    input_df["doc_id"] = doc_ids
+    input_df["chunk_position"] = chunk_positions
+    input_df["text"] = chunk_texts
+    input_df["embeddings"] = chunk_embdeddings[0 : len(chunk_texts), :].tolist()
 
-    batch_size = int(len(docs_w_emb) / n_jobs) + (len(docs_w_emb) % n_jobs > 0)
+    query_dfs = [input_df[i : i + 2048] for i in range(0, input_df.shape[0], 2048)]
 
-    batches = [(
-        docs_w_emb[i:i + batch_size],
-        i // batch_size,
-        indexer
-    ) for i in range(0, len(docs_w_emb), batch_size)]
-    
-    # Apply the function to each batch in parallel
+    result_dfs = []
     logger.info("Generating candidates with Hybrid Search...")
-    multiprocessing.set_start_method('spawn')
-    with multiprocessing.Pool(processes=n_jobs) as pool:
-        results = pool.starmap(process_batch, batches)
+    for query_df in query_dfs:
+        result_dfs.append(
+            client.query_collection(
+                query_df=query_df,
+                collection_name=collection_name,
+                vector_dimensions=embedding_dim,
+                n_jobs=args.n_jobs,
+                n_hits=args.n_hits,
+                alpha=args.alpha,
+                hnsw_metric_function="array_cosine_distance",
+            )
+        )
+
+    result = pd.concat(result_dfs, ignore_index=True)
+    result = (
+        result.groupby(["doc_id", "label_id"])
+        .agg(
+            score=("score", "sum"),
+            occurrences=("doc_id", "count"),
+            first_occurence=("chunk_position", "min"),
+            last_occurence=("chunk_position", "max"),
+            spread=("chunk_position", lambda x: x.max() - x.min()),
+            is_prefLabel=("is_prefLabel", "any"),
+        )
+        .reset_index()
+    )
+    result = (
+        result.sort_values(["score"], ascending=False)
+        .groupby("doc_id")
+        .head(args.top_k)
+    )
+    result = pd.merge(result, n_chunks_df, on="doc_id", how="left")
+    result["score"] = result["score"] / result["n_chunks"]
+    result["first_occurence"] = result["first_occurence"] / result["n_chunks"]
+    result["last_occurence"] = result["last_occurence"] / result["n_chunks"]
+    result["spread"] = result["spread"] / result["n_chunks"]
+    result = result.drop("n_chunks", axis=1)
 
     logger.info("Writing Outputs...")
-    # Concatenate the results
-    inner_results_list = [pd.concat(result) for result in results if result is not None]
-    df_results = pd.concat([result for result in inner_results_list if not result.empty])
-
-    # Write results arrow file to 'output'
-    df_results.to_feather(output)
-
-
+    result.to_feather(args.output)
