fc53f042a4c2d0234ea216a2a67773cf1cfe2e4e
Clemens Rietdorf <c.rietdorf@dnb.de>
2025-06-04 12:05:47 +0200

    replace pandas by polars

--- ebm4subjects/prepare_data.py
+++ ebm4subjects/prepare_data.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 import numpy as np
-import pandas as pd
+import polars as pl
 import pyoxigraph
 
 from ebm4subjects.embedding_generator import EmbeddingGenerator
@@ -10,21 +10,27 @@ PREF_LABEL_IRI = "http://www.w3.org/2004/02/skos/core#prefLabel"
 ALT_LABEL_IRI = "http://www.w3.org/2004/02/skos/core#altLabel"
 
 
-def parse_vocab(ttl_path: Path, use_altLabels: bool = True) -> pd.DataFrame:
+def parse_vocab(ttl_path: Path, use_altLabels: bool = True) -> pl.DataFrame:
     with ttl_path.open("rb") as in_file:
         graph = pyoxigraph.parse(input=in_file, format=pyoxigraph.RdfFormat.TURTLE)
-        labels: list[(str, str, bool)] = []
+        idns = []
+        label_texts = []
+        pref_labels = []
 
         for identifier, pref_alt, label, _ in graph:
             idn = identifier.value.split("/")[-1]
             label_text = label.value
 
             if pref_alt.value == PREF_LABEL_IRI:
-                labels.append((idn, label_text, True))
+                idns.append(idn)
+                label_texts.append(label_text)
+                pref_labels.append(True)
             elif pref_alt.value == ALT_LABEL_IRI and use_altLabels:
-                labels.append((idn, label_text, False))
+                idns.append(idn)
+                label_texts.append(label_text)
+                pref_labels.append(True)
 
-    return pd.DataFrame(labels, columns=["idn", "label_text", "is_prefLabel"])
+    return pl.DataFrame({"idn": idns, "label_text": label_texts, "is_prefLabel": pref_labels})
 
 
 def create_vocab_embeddings(
@@ -37,12 +43,12 @@ def create_vocab_embeddings(
     use_altLabels: bool = True,
 ):
     vocab = parse_vocab(ttl_path, use_altLabels)
-    vocab.to_feather(vocab_out)
+    vocab.write_ipc(vocab_out)
 
     generator = EmbeddingGenerator(model_name, embedding_dimensions)
 
     embeddings = generator.generate_embeddings(
-        vocab["label_text"].tolist(),
+        vocab.get_column("label_text").to_list(),
         batch_size=batch_size,
         task="retrieval.query",
     )
