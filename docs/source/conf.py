6ca1a4c18bee6fee14e38f703a2dedf52f89fd17
Clemens Rietdorf <c.rietdorf@dnb.de>
2025-10-07 12:06:47 +0200

    update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -6,17 +9,17 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'ebm4subjects'
-copyright = '2025, Deutsche Nationalbibliothek'
-author = 'Deutsche Nationalbibliothek'
-release = '0.3.1'
+project = "ebm4subjects"
+copyright = "2025, Deutsche Nationalbibliothek"
+author = "Deutsche Nationalbibliothek"
+release = "0.4.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc',  'sphinx.ext.napoleon', 'recommonmark']
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "recommonmark"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 # -- Path setup --------------------------------------------------------------
@@ -24,13 +27,10 @@ exclude_patterns = []
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../../src/ebm4subjects'))
+sys.path.insert(0, os.path.abspath("../../src/ebm4subjects"))
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
